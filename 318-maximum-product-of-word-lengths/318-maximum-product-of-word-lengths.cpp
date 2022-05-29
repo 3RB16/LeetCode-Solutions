a45f9364e7b353b3f91c6da28445426cf29a8315
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-05-29 03:25:24 +0200

    Time: 45 ms (91.00%), Space: 15.6 MB (79.69%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int maxProduct(vector<string>& words) {
-        unordered_map<int , unordered_map<char , bool>> frequencylist;
-        int ans = 0 , index = 0;
-        for(auto &word : words){
-           unordered_map<char , bool> frequency;
-           for(auto &c : word)frequency[c] = true;
-           frequencylist[index++] = frequency;
-        }
-        for(int i = 0;i < words.size();i++){
-           for(int j = i + 1;j < words.size();j++){
-            bool valid = true;
-            for(char c = 'a';c <= 'z';c++) {
-             if(frequencylist[i][c] && frequencylist[j][c]){
-               valid = false;
-               break;
-             }
-            }
-            if(valid){
-             ans = max(ans , (int)words[i].size() * (int)words[j].size());
-            }
-           }
-        }
-        return ans;
-    }
+   int maxProduct(vector<string>& words) {
+	int n = (int)words.size() , ans = 0;
+	vector<int> mask(n);
+	for(int i = 0; i < n; i++) {
+		for(auto& ch : words[i])
+			mask[i] |= 1 << (ch - 'a');
+		for(int j = 0; j < i; j++)
+			if((mask[i] & mask[j]) == 0)
+				ans = max(ans, (int)words[i].size() * (int)words[j].size());
+	}
+	return ans;
+   }
 };
+
