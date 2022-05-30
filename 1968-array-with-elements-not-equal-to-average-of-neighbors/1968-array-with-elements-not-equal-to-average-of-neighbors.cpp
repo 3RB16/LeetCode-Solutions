a55870537e750d4adf38b02d244bf1c0d0beb7f8
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-05-30 16:08:50 +0200

    Time: 397 ms (19.80%), Space: 128.4 MB (6.93%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -3,14 +3,12 @@ public:
   vector<int> rearrangeArray(vector<int>& nums){
         vector<int> ans;
         sort(nums.begin(),nums.end());
-        int s=0,e=nums.size()-1;
-        while(s<=e){
-            ans.push_back(nums[s]);
-            ans.push_back(nums[e]);
-            s++;
-            e--;
+        int s = 0,e = nums.size() - 1;
+        while(s <= e){
+            ans.push_back(nums[s++]);
+            ans.push_back(nums[e--]);
         }
-        if(ans.size()!=nums.size()){
+        if(ans.size() != nums.size()){
             ans.pop_back();
         }
         return ans;
