1ff09ec767e1489581b1da72938bb07535cc3bf1
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-06-17 22:59:38 +0200

    Time: 157 ms (93.89%), Space: 98.1 MB (58.27%) - LeetHub

--- 1734-decode-xored-permutation/1734-decode-xored-permutation.cpp
+++ 1734-decode-xored-permutation/1734-decode-xored-permutation.cpp
@@ -4,11 +4,10 @@ public:
        int n = (int)encoded.size() + 1;
        vector <int> perm(n , 0);
        int xors = 0;
-       for(int item = 1;item <= n;item++){
+       for(int item = 1;item <= n;item++)
            xors ^= item;
-           if(item < n && item % 2 == 1)
-               xors ^= encoded[item];
-       }
+       for(int i = 1;i < n - 1;i += 2)
+           xors ^= encoded[i];
        perm[0] = xors;
        for(int i = 1;i < n;i += 1)
            perm[i] = (perm[i - 1] ^ encoded[i - 1]);
