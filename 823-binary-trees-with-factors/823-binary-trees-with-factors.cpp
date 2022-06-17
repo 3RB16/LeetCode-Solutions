2db28a44e4ad99b34fc4dfcd3e3efa5e4e2711e0
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-06-17 04:40:55 +0200

    Time: 165 ms (27.09%), Space: 10.3 MB (36.65%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    map<int , long long> dp;
+    set <int> factors;
+    static const int MOD = 1e9 + 7;
+    long long ways(int value){
+        if(dp.count(value))return dp[value];
+        long long count = 1;
+        for(auto factor : factors)
+            if(value % factor == 0 and factors.count(value / factor))
+                count += ways(factor) * ways(value / factor);
+        return dp[value] = count % MOD;
+    }
+    int numFactoredBinaryTrees(vector<int>& arr) {
+      for(auto item : arr)
+          factors.insert(item);
+      long long ans = 0;
+      for(auto factor : factors){
+          ans += ways(factor);
+          ans %= MOD;
+      }
+      return ans;
+    }
+};
