8a410a329f6527b83bb9a113eb56d63112d22fd5
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-06-18 23:35:53 +0200

    Time: 728 ms (50.76%), Space: 150.1 MB (68.05%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,25 @@
+class WordFilter {
+   private:
+    unordered_map<string, int> position;
+
+   public:
+    WordFilter(vector<string>& words) {
+        int n = words.size();
+        for (int i = 0; i < n; i++) {
+            string word = words[i];
+            int wordSize = word.size();
+            for (int j = 1; j <= wordSize; j++) {
+                string p = word.substr(0, j);
+                for (int k = 0; k < wordSize; k++) {
+                    string s = word.substr(k, wordSize);
+                    position[p + "|" + s] = i + 1;
+                }
+            }
+        }
+    }
+
+    int f(string prefix, string suffix) {
+        string s = prefix + "|" + suffix;
+        return position[s] - 1;
+    }
+};
