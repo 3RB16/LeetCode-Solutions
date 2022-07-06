622a5081b89cadc033422649acafc7040ed22a5d
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-07-06 13:45:56 +0200

    Time: 0 ms (100.00%), Space: 6 MB (18.24%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int fib(int n) {
+        map<int , int> fib;
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i = 2;i <= n;i++)fib[i] = fib[i - 1] + fib[i - 2];
+        return fib[n];
+    }
+};
