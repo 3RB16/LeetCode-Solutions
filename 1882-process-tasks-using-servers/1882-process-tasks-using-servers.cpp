fefbc5b6d4c937663aeb83c398db6e920c97e96e
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-07-06 22:25:15 +0200

    Time: 764 ms (65.50%), Space: 132 MB (33.40%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.cpp
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.cpp
@@ -1,22 +1,27 @@
 class Solution {
   public:
     vector<int> assignTasks(vector<int>& servers, vector<int>& tasks) {
-        priority_queue<array<long, 3>, vector<array<long, 3>>,
-                       greater<array<long, 3>>> available_servers, busy_servers;
+        priority_queue<pair<long , long> ,  vector<pair<long , long>> ,
+                       greater<pair<long , long>>> available_servers, busy_servers;
         for(int index = 0; index < (int)servers.size(); ++index) {
-            available_servers.push({0, servers[index], index});
+            available_servers.push({servers[index], index});
         }
         vector <int> answer(tasks.size(), 0);
+        long seconds = 0;
         for(long index = 0; index < tasks.size(); ++index) {
-            while(!busy_servers.empty() && (busy_servers.top()[0] <= index || available_servers.empty())) {
-                auto [time, weight, i] = busy_servers.top();
-                busy_servers.pop();
-                available_servers.push({time <= index ? 0 : time, weight, i});
+            seconds = max(seconds , index);
+            if(available_servers.empty() && busy_servers.top().first > seconds){
+                seconds = busy_servers.top().first;
             }
-            auto [time, weight, i] = available_servers.top();
+            while(!busy_servers.empty() && busy_servers.top().first <= seconds){
+                auto [time , idx] = busy_servers.top();
+                available_servers.push({servers[idx] , idx});
+                busy_servers.pop();
+            }
+            auto [time , idx] = available_servers.top();
+            busy_servers.push({seconds + tasks[index] , idx});
+            answer[index] = idx;
             available_servers.pop();
-            busy_servers.push({max(time, index) + tasks[index], weight, i});
-            answer[index] = i;
         }
         return answer;
     }
