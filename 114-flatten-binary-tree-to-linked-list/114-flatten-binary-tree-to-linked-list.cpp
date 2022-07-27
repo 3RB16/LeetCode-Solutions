ef9bf0463b40abda1db1d79c0a494b7e137d3364
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-07-27 10:16:18 +0200

    Time: 11 ms (51.26%), Space: 13.2 MB (6.68%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    TreeNode* queue[2000];
-    int pos = 0;
+    vector<TreeNode*> queue;
     void dfs(TreeNode* root) {
         if (!root) 
             return;
-        queue[pos++] = root;
+        queue.push_back(root);
         dfs(root->left);
         dfs(root->right);
     }
     void flatten(TreeNode* root) {
         dfs(root);
-        for (int i = 1; i < pos; i++) {
+        for (int i = 1; i < (int)queue.size(); i++) {
             queue[i - 1]->right = queue[i];
             queue[i - 1]->left = NULL;
         }
