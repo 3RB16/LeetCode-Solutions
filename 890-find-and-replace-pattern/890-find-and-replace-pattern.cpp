01973e1df86adb1c211fefdfed8db79da587e228
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-07-29 02:13:37 +0200

    Time: 12 ms (18.71%), Space: 8.7 MB (29.81%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector <int> paint(string current) {
+        int color = 1;
+        unordered_map <char , int> seen;
+        vector <int> to_return;
+        for(int i = 0;i < int(current.size());i++){
+            if(!seen[current[i]])
+                seen[current[i]] = color++;
+            to_return.push_back(seen[current[i]]);
+        }
+        return to_return;
+    }
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector <int> target = paint(pattern);
+        vector <string> answer;
+        for(const string word : words){
+            vector <int> current = paint(word);
+            if(current == target)
+                answer.push_back(word);
+        }
+        return answer;
+    }
+};
