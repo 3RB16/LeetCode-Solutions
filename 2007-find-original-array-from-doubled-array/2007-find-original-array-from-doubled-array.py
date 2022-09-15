af0d64b8564df3cfd6e95148f456bef27b3a1604
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-09-15 08:28:07 +0200

    Time: 3546 ms (8.65%), Space: 36.2 MB (6.00%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,16 +1,14 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
         changed.sort()
-        seen = {}
-        for value in changed:
-            seen[value] = seen.get(value , 0) + 1
+        seen = Counter(changed)
         answer = []
         for value in changed:
             if seen[value] == 0:
                 continue
             answer.append(value)
-            seen[value] = seen.get(value , 0) - 1
-            seen[2 * value] = seen.get(2 * value , 0) - 1
+            seen[value] -= 1
+            seen[2 * value] -= 1
         if len(answer) * 2 != len(changed):
             answer = []
         return answer
