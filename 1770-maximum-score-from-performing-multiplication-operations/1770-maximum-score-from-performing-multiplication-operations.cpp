6a53d9f65e4a5c9cba331ea99e15c76a1c9036ac
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-09-16 05:18:02 +0200

    Time: 927 ms (22.57%), Space: 120 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
-        
-        if (j == M.size()) return 0;
-        if (dp[i][j] != INT_MIN) return dp[i][j];
-        
-        // Left Side
-        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
-        
-        // Right Side
-        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
-        
-        return dp[i][j] = max(left, right);
+    vector<vector<int>> memo;
+    int n , m;
+    int solve(int i , int j , vector <int>&multipliers , vector <int>&nums) {
+        if(i == m)
+            return 0;
+        if(memo[i][j] != INT_MIN)
+            return memo[i][j];
+        int last = n - 1 - (i - j);
+        int pickFirst = solve(i + 1 , j + 1 , multipliers , nums) 
+            + (multipliers[i] * nums[j]);
+        int pickSecond = solve(i + 1 , j , multipliers , nums) 
+            + (multipliers[i] * nums[last]);
+        return memo[i][j] = max(pickFirst , pickSecond);
     }
-    
-    int maximumScore(vector<int>& nums, vector<int>& M) {   
-        int n = nums.size(), m = M.size();
-        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, n, 0, nums, M);
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        n = int(nums.size()) , m = int(multipliers.size());
+        memo = vector<vector<int>> (m + 1, vector<int>(m + 1, INT_MIN));
+        return solve(0 , 0 , multipliers , nums);
     }
 };
