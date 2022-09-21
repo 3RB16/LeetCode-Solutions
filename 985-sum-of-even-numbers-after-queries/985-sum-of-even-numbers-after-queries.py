c079e3c7c79efd82ad26f7f576da74972035bc63
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-09-21 11:44:58 +0200

    Time: 431 ms (92.31%), Space: 19.6 MB (78.21%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -3,19 +3,19 @@ class Solution(object):
         """
         :type nums: List[int]
         :type queries: List[List[int]]
-        :rtype: List[int]
+        :retype: List[int]
         """
         summation = 0
         for num in nums:
             if num % 2 == 0:
                 summation += num
         answer = []
-        for query in queries:
-            if nums[query[1]] % 2 == 0:
-              summation -= nums[query[1]]
-            nums[query[1]] += query[0]
-            if nums[query[1]] % 2 == 0:
-                summation += nums[query[1]]
+        for [value , index] in queries:
+            if nums[index] % 2 == 0:
+                summation -= nums[index]
+            nums[index] += value
+            if nums[index] % 2 == 0:
+                summation += nums[index]
             answer.append(summation)
         return answer
         
