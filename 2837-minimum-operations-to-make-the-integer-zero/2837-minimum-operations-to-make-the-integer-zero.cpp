33361f293339f49672317c7b7e7782e83e1296e3
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2023-06-29 03:17:24 +0300

    Time: 5 ms (35.01%), Space: 6 MB (14.54%) - LeetHub

---
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,33 @@
+/**
+  @1 - AC from first time [super testing].
+  @2 - Clean Code.
+  @3 - Speed.
+**/
+
+class Solution { 
+public: 
+    int countBits(long long num) { 
+        int count = 0; 
+        while (num > 0) { 
+            count += num & 1; 
+            num >>= 1; 
+        } 
+        return count; 
+    } 
+
+    int makeTheIntegerZero(int num1, int num2) { 
+        if (num1 < num2) { 
+            return -1; 
+        } 
+ 
+        for (int steps = 0; steps <= 100; steps++) { 
+            long long diff = num1 - 1LL*(num2) * steps; 
+            int bits = countBits(diff); 
+            if (bits <= steps && steps <= diff) { 
+                return steps; 
+            } 
+        } 
+ 
+        return -1; 
+    }
+};
