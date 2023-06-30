de2f4b9a70c09ad52bd92407e77ecf1af3204b14
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2023-07-01 02:06:14 +0300

    Time: 0 ms (100%), Space: 6.1 MB (22.28%) - LeetHub

--- 2850-construct-the-longest-new-string/2850-construct-the-longest-new-string.cpp
+++ 2850-construct-the-longest-new-string/2850-construct-the-longest-new-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestString(int x, int y, int z) {
-       int minimum = min(x, y);
+       int minimum = (x < y ? x : y);
        return (z << 1) + (minimum << 2) + ((x != y) << 1);
     }
 };
