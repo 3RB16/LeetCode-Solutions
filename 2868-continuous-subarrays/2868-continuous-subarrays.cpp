bd1c133b11dcf2c967c15a4a5e6a5937f18c5693
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2023-07-02 12:02:59 +0300

    Time: 377 ms (38.46%), Space: 150 MB (7.69%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,72 +1,20 @@
-/**
-  @1 - AC from first time [super testing].
-  @2 - Clean Code.
-  @3 - Speed.
-**/
-struct SegmentTreeNode {
-    int min_val;
-    int max_val;
-};
-
-struct SegmentTree {
-    vector<int> arr;
-    vector<SegmentTreeNode> tree;
-    int n;
-
-    SegmentTree(const vector<int>& nums) {
-        arr = nums;
-        n = nums.size();
-        tree.resize(4 * n);
-        build(0, 0, n - 1);
-    }
-
-    void build(int node, int start, int end) {
-        if (start == end) {
-            tree[node].min_val = arr[start];
-            tree[node].max_val = arr[start];
-        } else {
-            int mid = (start + end) / 2;
-            build(2 * node + 1, start, mid);
-            build(2 * node + 2, mid + 1, end);
-            tree[node].min_val = min(tree[2 * node + 1].min_val, tree[2 * node + 2].min_val);
-            tree[node].max_val = max(tree[2 * node + 1].max_val, tree[2 * node + 2].max_val);
-        }
-    }
-
-    SegmentTreeNode query(int node, int start, int end, int left, int right) {
-        if (left > end || right < start) {
-            return {INT_MAX, INT_MIN};
-        }
-        if (left <= start && right >= end) {
-            return tree[node];
-        }
-        int mid = (start + end) / 2;
-        SegmentTreeNode left_child = query(2 * node + 1, start, mid, left, right);
-        SegmentTreeNode right_child = query(2 * node + 2, mid + 1, end, left, right);
-        return {
-            min(left_child.min_val, right_child.min_val),
-            max(left_child.max_val, right_child.max_val)
-        };
-    }
-};
-
-
 class Solution {
-  public:
+public:
     long long continuousSubarrays(vector<int>& nums) {
-        int count = 0;
-        SegmentTree st(nums);
-        int n = int(nums.size());
+        int rear = 0;
         long long ans = 0;
-        int p1 = 0;
-        for(int p2 = 1; p2 < n; p2++)while(p1 < p2) {
-                SegmentTreeNode res = st.query(0, 0, st.n - 1, p1, p2 - 1);
-                int mini = res.min_val , maxi = res.max_val;
-                if(abs(nums[p2] - mini) <= 2 && abs(nums[p2] - maxi) <= 2) break;
-                ans += p2 - p1;
-                p1++;
+        multiset<int> st;
+        
+        for(int front = 0; front < int(nums.size()); front++){
+          st.insert(nums[front]);
+            
+          while(st.size() > 1 && *st.rbegin() - *st.begin() > 2){
+            st.erase(st.find(nums[rear++]));
+          }
+          
+          ans += front - rear + 1;
         }
-        long long left = n - p1;
-        return ans + (left *1LL* (left + 1) >> 1);
+        
+        return ans;
     }
 };
