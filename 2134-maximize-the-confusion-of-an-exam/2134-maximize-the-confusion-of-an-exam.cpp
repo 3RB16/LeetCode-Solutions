6c901bf80b633fe22e1183019e44bfbd616d667d
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2023-07-07 04:26:55 +0300

    Time: 30 ms (52.43%), Space: 10 MB (95.61%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int max_answers = 0;
-        int n = int(answerKey.size());
+      int max_answers = 0;
+      int n = int(answerKey.size());
 
-        for(const char t : {'T' , 'F'}) {
-          int begin = 0 , end = 0;
-          int cur_k = k;
-          while(end < n) {
-            if(answerKey[end] != t)
-              cur_k--;
-            while(cur_k < 0){
-              if(answerKey[begin] != t)
-                ++cur_k;
-              ++begin;
+      for(const char target : {'T' , 'F'}) {
+        int begin = 0 , end = 0;
+        int cur_k = k;
+        while(end < n) {
+          if(answerKey[end] != target){
+            cur_k--;
+          }
+          while(cur_k < 0){
+            if(answerKey[begin] != target){
+              ++cur_k;
             }
-            max_answers = max(max_answers , end - begin + 1); 
-            ++end;
-          }          
-        }
-        return max_answers;
+            ++begin;
+          }
+          max_answers = max(max_answers , end - begin + 1); 
+          ++end;
+        }          
+      }
+      return max_answers;
     }
 };
