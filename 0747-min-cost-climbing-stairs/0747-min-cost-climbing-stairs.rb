bc486f74b82ab72ab2d527e78dc0c62e198cec1b
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2023-10-13 16:34:02 +0300

    Time: 54 ms (91.58%), Space: 211.6 MB (44.21%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} cost
+# @return {Integer}
+def min_cost_climbing_stairs(cost)
+    @n = cost.length
+    @memo = Array.new(@n , nil)
+    @cost = cost
+    return [solve(0) , solve(1)].min
+end
+
+def solve(steps)
+  return 0 if steps >= @n
+  return @memo[steps] if @memo[steps]
+
+  @memo[steps] = @cost[steps] + [solve(steps + 1) , solve(steps + 2)].min
+  return @memo[steps]
+end
