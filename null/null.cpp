af335f5a5477cef9f05bbe83a470c2dce677042a
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2024-03-20 23:00:06 +0200

    Update file 3/20/2024, 11:00:05 PM

--- null/null.cpp
+++ null/null.cpp
@@ -1,3 +1,7 @@
+            if (!marked[index]){
+                sum -= nums[index] , marked[index] = true;
+                unmarked.erase(make_pair(nums[index] , index));
+            }
             while(!unmarked.empty() && value-- > 0) {
                 auto [value , index] = *unmarked.begin();
                 sum -= value, marked[index] = true;
@@ -8,8 +12,4 @@
         return ans;
     }
 };
-            if (!marked[index]){
-                sum -= nums[index] , marked[index] = true;
-                unmarked.erase(make_pair(nums[index] , index));
-            }
 [1,2,2,1,2,3,1]
