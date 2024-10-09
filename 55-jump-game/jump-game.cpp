4117d0d67e854fe56e85bbb67222ca93fde5ffde
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2024-10-09 04:03:09 +0300

    Time: 35 ms (91.89%) | Memory: 51.1 MB (80.16%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = 0;
+        for(int i = 0;i < int(nums.size());i++) {
+            if(reach < i) {
+                return false;
+            }
+            reach = max(reach, i + nums[i]);
+        }
+        return true;
+    }
+};
